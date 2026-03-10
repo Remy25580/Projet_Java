fef1da34fb2e9a184691a6e6f18ae710acb1668e
Remy25580 <remy.martinot@coda-student.school>
2026-03-10 14:17:16 +0100

    feat: changement de l'attribut occupied de la classe Case

--- Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/tools/Case.java
+++ Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/tools/Case.java
@@ -1,24 +1,26 @@
 package school.coda.remy_axel_ethan.projet_java.tools;
 
+import school.coda.remy_axel_ethan.projet_java.boat.Boat;
+
 public class Case {
-    private boolean occupied;
+    private Boat occupiedBy;
     private boolean touched;
     private final int xPos;
     private final int yPos;
 
     public Case(int x, int y){
-        this.occupied = false;
+        this.occupiedBy = null;
         this.touched = false;
         this.xPos = x;
         this.yPos = y;
     }
 
-    public boolean getOccupied(){return this.occupied;}
+    public Boat getOccupiedBy(){return this.occupiedBy;}
     public boolean getTouched(){return this.touched;}
     public int[] getPos(){
         return new int[]{this.xPos, this.yPos};
     }
 
-    public void changeOccupied(){this.occupied = !this.occupied;}
+    public void setOccupiedBy(Boat boat){this.occupiedBy = boat;}
     public void changeTouched(){this.touched = !this.touched;}
 }
