aa1a02a2ff6a077276c781792239f4835faceefa
Remy25580 <remy.martinot@coda-student.school>
2026-03-12 14:24:07 +0100

    feat: création de la fonction pour toucher une case

--- Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/GameController.java
+++ Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/GameController.java
@@ -13,6 +13,8 @@ import school.coda.remy_axel_ethan.projet_java.tools.Case;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 import static school.coda.remy_axel_ethan.projet_java.tools.Grille.*;
 
@@ -35,6 +37,8 @@ public class GameController implements Initializable {
     private Label opponentGridTitle;
     @FXML
     private  Label yourGridTitle;
+    @FXML
+    private Label caseTouchedErrorMessage;
 
     private Boat selectedBoat = null;
     private Button currentBoatButton;
@@ -200,5 +204,33 @@ public class GameController implements Initializable {
         opponentGridTitle.setManaged(true);
         yourGridTitle.setVisible(true);
         yourGridTitle.setManaged(true);
+    }
+
+    public void shoot(Case target){
+        PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+        if (target.getTouched()){
+            caseTouchedErrorMessage.setText("Cette case a déjà été touchée !!");
+            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+            pause.play();
+            return;
+        }
+        target.changeTouched();
+        if(target.getOccupiedBy() != null){
+            target.getOccupiedBy().receiveDamage();
+            getButtonFromACase(target).setStyle("-fx-background-color: red;" +
+                    "-fx-border-color: black;" +
+                    "-fx-border-radius: 0;");
+            caseTouchedErrorMessage.setText(target.getOccupiedBy().getType() + " touché !");
+            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+            pause.play();
+        }else{
+            target.getOccupiedBy().receiveDamage();
+            getButtonFromACase(target).setStyle("-fx-background-color: gray;" +
+                    "-fx-border-color: black;" +
+                    "-fx-border-radius: 0;");
+            caseTouchedErrorMessage.setText("Aucune cible touchée . . .");
+            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+            pause.play();
+        }
     }
 }
