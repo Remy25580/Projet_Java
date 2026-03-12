a2730d57793badb15bda8ff3682b6e43377708af
EthanCarteron <ethan.carteron@gmail.com>
2026-03-12 14:00:27 +0100

    fix: problème de merge, ajout du chemin relatif dans initgame

--- Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/InitGame.java
+++ Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/InitGame.java
@@ -1,16 +1,15 @@
-package school.coda.remy_axel_ethan.projet_java.game.placement;
+package school.coda.remy_axel_ethan.projet_java.game;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 import java.io.IOException;
 
-public class BoatPlacement{
+public class InitGame {
     public void initPlacement(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BoatPlacement.class.getResource("placement-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(InitGame.class.getResource("placement/placement-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 650);
 
         stage.setTitle("Bataille Javale");
 
