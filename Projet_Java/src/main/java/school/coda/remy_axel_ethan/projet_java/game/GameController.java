75493bfcece8977553e9af1a5e06703ba3bfca5e
EthanCarteron <ethan.carteron@gmail.com>
2026-03-12 15:49:38 +0100

    refacto: creation de la classe creation boat pour libérer gamecontroller
    # Conflicts:
    #	Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/GameController.java

--- Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/GameController.java
+++ Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/GameController.java
@@ -13,6 +13,8 @@ import school.coda.remy_axel_ethan.projet_java.tools.Case;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 import static school.coda.remy_axel_ethan.projet_java.tools.Grille.*;
 
@@ -41,7 +43,7 @@ public class GameController implements Initializable {
     private Boat selectedBoat = null;
     private Button currentBoatButton;
     private boolean isHorizontal = true;
-    protected Case[][] cases;
+    private Case[][] cases;
     private int nbBoatPlaced = 0;
 
     @Override
@@ -203,5 +205,34 @@ public class GameController implements Initializable {
         yourGridTitle.setVisible(true);
         yourGridTitle.setManaged(true);
     }
+
+    public void shoot(Case target){
+        PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+        if (target.getTouched()){
+            caseTouchedErrorMessage.setText("Cette case a déjà été touchée !!");
+            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+            pause.play();
+            return;
+        }
+        target.changeTouched();
+        if(target.getOccupiedBy() != null){
+            target.getOccupiedBy().receiveDamage();
+            getButtonFromACase(target).setStyle("-fx-background-color: red;" +
+                    "-fx-border-color: black;" +
+                    "-fx-border-radius: 0;");
+            caseTouchedErrorMessage.setText(target.getOccupiedBy().getType() + " touché !");
+            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+            pause.play();
+        }else{
+            target.getOccupiedBy().receiveDamage();
+            getButtonFromACase(target).setStyle("-fx-background-color: gray;" +
+                    "-fx-border-color: black;" +
+                    "-fx-border-radius: 0;");
+            caseTouchedErrorMessage.setText("Aucune cible touchée . . .");
+            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+            pause.play();
+        }
+    }
+
 
 }
---
+++ Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/placement/CreationBoat.java
@@ -0,0 +1,93 @@
+package school.coda.remy_axel_ethan.projet_java.game.placement;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import school.coda.remy_axel_ethan.projet_java.boat.Boat;
+import school.coda.remy_axel_ethan.projet_java.tools.Case;
+
+import java.util.Arrays;
+
+import static school.coda.remy_axel_ethan.projet_java.tools.Grille.CASE_X_POSITION;
+import static school.coda.remy_axel_ethan.projet_java.tools.Grille.CASE_Y_POSITION;
+
+public class CreationBoat {
+
+    private final Label selectedBoatLabel;
+    private Boat selectedBoat;
+    private final BoardRules rules;
+    private Button currentBoatButton;
+    private final GridPane grid;
+    private final Button gameStartButton;
+    private int nbBoatPlaced;
+
+
+    public CreationBoat (Boat selectedBoat, BoardRules rules, Button currentBoatButton, GridPane grid, Label selectedBoatLabel, Button gameStartButton, int nbBoatPlaced) {
+        this.selectedBoat = selectedBoat;
+        this.rules = rules;
+        this.currentBoatButton = currentBoatButton;
+        this.grid = grid;
+        this.selectedBoatLabel = selectedBoatLabel;
+        this.gameStartButton = gameStartButton;
+        this.nbBoatPlaced = nbBoatPlaced;
+    }
+    public boolean tryPlacement(Case target) {
+        int x = target.getPos()[0];
+        int y = target.getPos()[1];
+        int size = selectedBoat.getSize();
+
+        if (!rules.isInGrid(x, y, size)) {
+            IO.println("Le bateau sort de la grille !");
+            return false;
+        }
+        if (!rules.isCasesFree(x, y, size)) {
+            return false;
+        }
+
+        applyPlacement(x, y, size);
+
+        if (currentBoatButton != null) {
+            currentBoatButton.setDisable(true);
+        }
+
+        this.selectedBoatLabel.setText("Bateau sélectionné : aucun");
+
+        return true;
+    }
+
+    private void applyPlacement(int x, int y, int size) {
+        for (int i = 0; i < size; i++) {
+            int placeX = rules.isHorizontal ? x + i : x;
+            int placeY = rules.isHorizontal ? y : y + i;
+
+            placeBoat(rules.cases[placeX][placeY]);
+        }
+    }
+
+    public void placeBoat(Case target){
+        target.setOccupiedBy(selectedBoat);
+        IO.println(Arrays.toString(target.getPos()));
+        getButtonFromACase(target).setStyle("-fx-background-color: red;" +
+                "-fx-border-color: black;" +
+                "-fx-border-radius: 0;");
+    }
+
+    public Button getButtonFromACase(Case target){
+        Button button = null;
+        int xButton;
+        int yButton;
+        int xTarget = target.getPos()[0];
+        int yTarget = target.getPos()[1];
+
+        for(Node node : grid.getChildren()){
+            button = (Button) node;
+            xButton = (int) button.getProperties().get(CASE_X_POSITION);
+            yButton = (int) button.getProperties().get(CASE_Y_POSITION);
+            if(xButton == xTarget && yButton == yTarget){
+                return button;
+            }
+        }
+        return button;
+    }
+}
