45637f2472407f0e6d07bd5714931ed6c4a3e5ba
EthanCarteron <ethan.carteron@gmail.com>
2026-03-12 16:09:30 +0100

    fix: merge conflit réparés

--- Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/ingame/Attack.java
+++ Projet_Java/src/main/java/school/coda/remy_axel_ethan/projet_java/game/ingame/Attack.java
@@ -6,33 +6,32 @@ import school.coda.remy_axel_ethan.projet_java.game.GameController;
 import school.coda.remy_axel_ethan.projet_java.tools.Case;
 
 public class Attack extends GameController {
-
-    protected void shoot(Case target){
-        PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
-        if (target.getTouched()){
-            caseTouchedErrorMessage.setText("Cette case a déjà été touchée !!");
-            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
-            pause.play();
-            return;
-        }
-        target.changeTouched();
-        if(target.getOccupiedBy() != null){
-            target.getOccupiedBy().receiveDamage();
-            getButtonFromACase(target).setStyle("-fx-background-color: red;" +
-                    "-fx-border-color: black;" +
-                    "-fx-border-radius: 0;");
-            caseTouchedErrorMessage.setText(target.getOccupiedBy().getType() + " touché !");
-            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
-            pause.play();
-        }else{
-            target.getOccupiedBy().receiveDamage();
-            getButtonFromACase(target).setStyle("-fx-background-color: gray;" +
-                    "-fx-border-color: black;" +
-                    "-fx-border-radius: 0;");
-            caseTouchedErrorMessage.setText("Aucune cible touchée . . .");
-            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
-            pause.play();
-        }
-    }
-
+//
+//    protected void shoot(Case target){
+//        PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+//        if (target.getTouched()){
+//            caseTouchedErrorMessage.setText("Cette case a déjà été touchée !!");
+//            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+//            pause.play();
+//            return;
+//        }
+//        target.changeTouched();
+//        if(target.getOccupiedBy() != null){
+//            target.getOccupiedBy().receiveDamage();
+//            getButtonFromACase(target).setStyle("-fx-background-color: red;" +
+//                    "-fx-border-color: black;" +
+//                    "-fx-border-radius: 0;");
+//            caseTouchedErrorMessage.setText(target.getOccupiedBy().getType() + " touché !");
+//            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+//            pause.play();
+//        }else{
+//            target.getOccupiedBy().receiveDamage();
+//            getButtonFromACase(target).setStyle("-fx-background-color: gray;" +
+//                    "-fx-border-color: black;" +
+//                    "-fx-border-radius: 0;");
+//            caseTouchedErrorMessage.setText("Aucune cible touchée . . .");
+//            pause.setOnFinished(_ -> caseTouchedErrorMessage.setText(""));
+//            pause.play();
+//        }
+//    }
 }
